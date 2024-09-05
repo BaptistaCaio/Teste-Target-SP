32f7c13e75b46db80ed54c770a5887fecf1dfb74
BaptistaCaio <caiobsantana2001@gmail.com>
2024-09-05 15:41:25 -0300

    Add files via upload

---
+++ inverterstring.py
@@ -0,0 +1,12 @@
+def inverter_string(texto):
+    texto_invertido = ""
+    for i in range(len(texto) - 1, -1, -1):
+        texto_invertido += texto[i]
+    return texto_invertido
+
+entrada = input("Digite uma string para ser invertida: ")
+
+resultado = inverter_string(entrada)
+
+print(f"String original: {entrada}")
+print(f"String invertida: {resultado}")
